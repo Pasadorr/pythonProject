e25e2559648af039ca05ee97272d75e5649102cc
Pasadorr <168021670+Pasadorr@users.noreply.github.com>
2024-04-30 14:58:48 +0300

    Update Homework For Cycle.py

--- Homework For Cycle.py
+++ Homework For Cycle.py
@@ -1,9 +1,8 @@
 list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 cars_count = 0
 for i in list_:
-    for j in list_:
-        cars_count += 10
-        print(f'Значение: {cars_count}')
+    cars_count += 10
+    print(f'Значение: {cars_count}')
     print('Я езжу на автомобиле марки', i)
 
 
