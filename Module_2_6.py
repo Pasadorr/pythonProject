fde98540eef03fb41b481aa7cd28773c929b02cf
Pasadorr <168021670+Pasadorr@users.noreply.github.com>
2024-06-02 22:32:12 +0300

    Edited

--- Module_2_6.py
+++ Module_2_6.py
@@ -1,12 +1,12 @@
+
+
 def single_root_words(root_word, *other_words):
     same_words = []
-    for i in other_words:
-        if root_word(i) in other_words:
-            same_words.append(i)
-        if root_word(i) not in other_words:
-            continue
-        return same_words
-    result = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-    print(result)
-    result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-    print(result2)
+    for word in other_words:
+        if root_word.lower().count(word.lower()) or word.lower().count(root_word.lower()):
+            same_words.append(word)
+    return same_words
+
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
