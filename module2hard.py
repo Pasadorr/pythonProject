03551c26c80f4f64fa106111a2c518989cb891b9
Pasadorr <168021670+Pasadorr@users.noreply.github.com>
2024-06-02 18:48:05 +0300

    Edited

--- module2hard.py
+++ module2hard.py
@@ -2,12 +2,12 @@ keys = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 def generate_password(n):
     pairs = []
     n = int(n)
-    for i in range(1, 21):
+    for i in range(1, n):
         for j in range(i + 1, n + 1):
-            if (i + j) % n == 0:
+            if n % (i + j) == 0:
                 pairs += str(i) + str(j)
     return pairs
-for result in range(1, 21):
+for result in range(1, 20):
     result = int(input('Введите цифру от 3 до 20 включительно: '))
     if result < 3 or result > 20:
         print('Ты совершил ошибку, попробуй заново')
