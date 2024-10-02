65dc1a9ddb84eaba6d4364fefc3c37526c2c1c84
Pasadorr <168021670+Pasadorr@users.noreply.github.com>
2024-10-02 14:33:46 +0300

    Edited

---
+++ Module9_7.py
@@ -0,0 +1,16 @@
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if result < 2:
+            print("Составное")
+        else:
+            print("Простое")
+        return result
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+result = sum_three(2, 3, 6)
+print(result)
