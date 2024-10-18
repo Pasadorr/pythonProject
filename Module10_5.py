4749008105f029fa77b437e5660c2155e7e9b957
Pasadorr <168021670+Pasadorr@users.noreply.github.com>
2024-10-18 23:28:31 +0300

    Edited

---
+++ Module10_5.py
@@ -0,0 +1,32 @@
+import time
+import multiprocessing
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        while True:
+            line = file.readline()
+            if line == '':
+                break
+            all_data.append(line)
+
+if __name__ == "__main__":
+    for number in range(1, 5):
+        with open(f'file {number}.txt', 'w') as file:
+            for i in range(10**6):
+                file.write(f"This is line {i} in file {number}\n")
+
+            filenames = [f'file {number}.txt' for number in range(1, 5)]
+
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    linear_time = time.time() - start_time
+    print(f"Линейное время выполнения: {linear_time}")
+
+    # Многопроцессный вызов
+    start_time = time.time()
+    with multiprocessing.Pool() as pool:
+        pool.map(read_info, filenames)
+    parallel_time = time.time() - start_time
+    print(f"Многопроцессное время выполнения: {parallel_time}")
