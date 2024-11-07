5dc91a08a4b55c559183160a6e64f0b4f85a419a
Pasadorr <168021670+Pasadorr@users.noreply.github.com>
2024-11-07 22:17:26 +0300

    Edited

---
+++ Module16_2.py
@@ -0,0 +1,24 @@
+# Module16_1.py
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi import Query
+from fastapi.params import Path
+from typing import Annotated
+app = FastAPI()
+@app.get("/", response_class=HTMLResponse)
+async def read_main():
+    return "Главная страница"
+@app.get("/user/admin", response_class=HTMLResponse)
+async def read_admin():
+    return "Вы вошли как администратор"
+@app.get("/user/{user_id}", response_class=HTMLResponse)
+async def read_user(
+    user_id: Annotated[int, Path(gt=0, le=100, description="Enter User ID")]
+):
+    return f"Вы вошли как пользователь № {user_id}"
+@app.get("/user/{username}/{age}", response_class=HTMLResponse)
+async def read_user_info(
+    username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username")],
+    age: Annotated[int, Path(ge=18, le=120, description="Enter age")]
+):
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
